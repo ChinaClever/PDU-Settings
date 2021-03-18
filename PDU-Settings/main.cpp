917ae5540be668e4f895fee35f39a42630974084
luozhiyong <luozhiyong131@qq.com>
2021-03-18 16:09:01 +0800

    改配置文件名

--- PDU-Settings/main.cpp
+++ PDU-Settings/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-    QtSingleApplication a(QLatin1String("MasterPduTool"),argc, argv);
+    QtSingleApplication a(QLatin1String("PDU-Settings"),argc, argv);
     if(a.isRunning())  {
         MsgBox::critical(NULL, QObject::tr("程序已运行！"));
         a.sendMessage("raise_window_noop", 1000); //4s后激活前个实例
