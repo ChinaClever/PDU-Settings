bac52470e63c427d2607f4ca123c714b7aa8f017
luozhiyong <luozhiyong131@qq.com>
2021-05-07 13:04:47 +0800

    增加异常捕获

--- PDU-Settings/cores/test_corethread.cpp
+++ PDU-Settings/cores/test_corethread.cpp
@@ -49,13 +49,18 @@ bool Test_CoreThread::startProcess()
 
     exe += ".exe";
     mRead->mac = true;
-
     bool ret = checkNet();
     if(ret){
-        mProcess->close();
-        mProcess->start(exe);
-        ret = mProcess->waitForFinished(120*1000);
-        mLogs->updatePro(tr("网页设置功能退出"), ret , 1);
+        try {
+            mProcess->close();
+            mProcess->start(exe);
+            mLogs->updatePro(tr("网页设置功能启动"));
+            ret = mProcess->waitForFinished(240*1000);
+            mLogs->updatePro(tr("网页设置功能正常退出"), ret , 1);
+        } catch (...) {
+            ret = mLogs->updatePro(tr("网页设置功能异常退出"), false);
+            updateMacAddr(1);
+        }
     }
 
     return ret;
